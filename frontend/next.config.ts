e99a138873913e43721875aeec2e88f962ebc63c
nathaly <nathygarcia159@gmail.com>
2024-11-29 14:02:24 -0500

    inventorys

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "rds-inventorys.cxum28mo09ky.us-east-2.rds.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
